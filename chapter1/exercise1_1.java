73dfedff9756ae6adfc0535e5dccaa18f67ede17
hadidean <156311068+hadidean@users.noreply.github.com>
2024-09-23 12:26:04 +0800

    Add files via upload

---
+++ chapter1/exercise1_1.java
@@ -0,0 +1,10 @@
+package chapter1;
+
+public class exercise1_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is fun");
+
+    }
+}
