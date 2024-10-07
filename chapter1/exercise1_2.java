cc43da35181ba7654ca152389010543847355d82
hadidean <156311068+hadidean@users.noreply.github.com>
2024-10-08 00:43:46 +0800

    Add files via upload

---
+++ chapter1/exercise1_2.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+public class exercise1_2 {
+    public static void main(String[] args) {
+        int x = 0;
+    
+        while (x < 5) {
+            System.out.println("Programming is fun");
+            x++;  
+        }
+
+    }
+ 
+}
